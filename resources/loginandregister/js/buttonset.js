3f8dc746dd67c18a82d3340e32dccc09d968e70d
MiRECoFu <1027066037@qq.com>
2016-10-13 11:04:11 +0800

    注册卡界面目前更新版本
    1）未完成功能：①密码、确认密码填写框输入时type类型转换为password以及两次输入内容不一致时报错②验证码发送按钮点击后开始倒计时120秒可重新点击
    2）未完成内容：登录界面

--- resources/loginandregister/js/buttonset.js
+++ resources/loginandregister/js/buttonset.js
@@ -1,7 +1,6 @@
 window.onload=buttonset;
 function buttonset(){
-  //整张注册卡出现的动画演示
-  $('registerCard').show(500);
+
   //选项卡选取国家区号特效设置
   var $telnum = $('#telnum'),
       $telform = $('.telchoose'),
@@ -15,7 +14,7 @@ function buttonset(){
     }else{
          e.cancelBubble=true;
     }
-    $telform.slideDown(100);
+    $telform.slideToggle(100);
   })
   $tellis.on({
     mouseover: function(e){
@@ -51,7 +50,8 @@ function buttonset(){
       $chenumtime = $('#chenum-time'),
       $registerIn = $('.registerIn'),
       $paswd = $('#paswd'),
-      $repaswd = $('#repaswd');
+      $repaswd = $('#repaswd'),
+      $relog = $('.relog');
 //填写框类型鼠标滑过红色填充阴影
  $input01.on({
    focusin: function(e){$(this).css('border-color','#f43e4b')},
@@ -59,11 +59,20 @@ function buttonset(){
 });
 //按钮鼠标划过按钮变色
 $chenumtime.on({
-  mouseover: function(e){$(this).css('background-color','#cd5555')},
+  mouseover: function(e){$(this).css('background-color','#fa8072')},
   mouseout: function(e){$(this).css('background-color','#f43e4b')}
 })
 $registerIn.on({
-  mouseover: function(e){$(this).css({'border-color':'#cd5555','color':'#cd5555'})},
+  mouseover: function(e){$(this).css({'border-color':'#fa8072','color':'#fa8072'})},
   mouseout: function(e){$(this).css({'border-color':'#f43e4b','color':'#f43e4b'})}
+})
+//密码确认密码填写框输入type变换以及如果两次输入内容不一致报错
+
+//验证码发送按钮点击开始倒计时设置
+
+//返回登陆按钮设置
+$relog.on({
+  mouseover: function(e){$(this).css('color','#f43e4b')},
+  mouseout: function(e){$(this).css('color','#fa8072')}
 })
 }
