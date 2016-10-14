554dbcbef99c9521831f904b703078b5aaf2e18f
CXWorks <1043806046@qq.com>
2016-10-15 00:15:22 +0800

    db

--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -6,6 +6,7 @@ try {
     (new Dotenv\Dotenv(__DIR__.'/../'))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
     //
+    echo $e;
 }
 
 /*
@@ -23,9 +24,9 @@ $app = new Laravel\Lumen\Application(
     realpath(__DIR__.'/../')
 );
 
-// $app->withFacades();
+$app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
