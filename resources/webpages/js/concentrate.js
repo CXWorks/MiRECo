0d3665422432ee587714cfafa049bf5737a77007
CXWorks <1043806046@qq.com>
2016-10-31 19:28:33 +0800

    concentrate

---
+++ resources/webpages/js/concentrate.js
@@ -0,0 +1,51 @@
+function concentrate(source,target) {
+  $.ajax({
+    type:'post',
+    url:'../../server/public/index.php/concentrate',
+    data:{
+      'method':'concentrate',
+      'sourceUserphone':source,
+      'targetUserphone':target
+    },
+    success:function (data,status) {
+      if (data['msg']=='success') {
+        alert('关注成功');
+      }else {
+      }
+    }
+  });
+}
+
+function unconcentrate(source,target) {
+  $.ajax({
+    type:'post',
+    url:'../../server/public/index.php/concentrate',
+    data:{
+      'method':'cancelconcentrate',
+      'sourceUserphone':source,
+      'targetUserphone':target
+    },
+    success:function (data,status) {
+      if (data['msg']=='success') {
+        alert('取消关注成功');
+      }else {
+      }
+    }
+  });
+}
+
+function concentrateList(source) {
+  $.ajax({
+    type:'get',
+    url:'../../server/public/index.php/concentrate',
+    data:{
+      'userphone':source
+    },
+    success:function (data,status) {
+      for (var vari in object) {
+        tar=vari['target'];
+        time=vari['date'];
+      }
+    }
+  });
+}
