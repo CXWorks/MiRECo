d4e272d1ecb8ebcf4810fd0e282028df32d178b5
MiRECoFu <1027066037@qq.com>
2016-11-10 13:35:54 +0800

    我的展览馆

--- resources/webpages/js/mygallery.js
+++ resources/webpages/js/mygallery.js
@@ -1,10 +1,23 @@
 $(window).on('load',function(){
+  whchange();
   //logoanimate();
   pagescroll();
-  var $thbx = $('.thbx');
-  var bxh = $thbx.width()*1.2;
-  $thbx.css('height',bxh);
 })
+//页面分辨率变化时的盒子宽高变化
+function whchange(){
+  if($(window).width() > 600) {
+    var $thbx = $('.thbx');
+    var bxw;
+    if($(window).width() <768){
+      bxw = $(window).width()/2;
+    }else {
+      bxw = $(window).width()/3;
+    }
+    var bxh = bxw*0.9;
+    $thbx.css('height',bxh);
+    }
+    setTimeout(whchange,1);
+}
 
 //页面滚动背景色渐变特效
 function pagescroll(){
