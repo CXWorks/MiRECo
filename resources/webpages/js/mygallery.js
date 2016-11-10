877f27f72412d8b77849797ae1bf70c218a3e06b
MiRECoFu <1027066037@qq.com>
2016-11-10 16:28:59 +0800

    更新了我的展览馆的box模型鼠标划过事件

--- resources/webpages/js/mygallery.js
+++ resources/webpages/js/mygallery.js
@@ -2,24 +2,58 @@ $(window).on('load',function(){
   whchange();
   //logoanimate();
   pagescroll();
+  boxtips();
 })
 //页面分辨率变化时的盒子宽高变化
 function whchange(){
   if($(window).width() > 600) {
     var $thbx = $('.thbx');
-    var bxw;
-    if($(window).width() <768){
-      bxw = $(window).width()/2;
-    }else {
-      bxw = $(window).width()/3;
-    }
-    var bxh = bxw*0.9;
+    var bxw = $thbx.width();
+    var bxh = bxw*1.3;
     $thbx.css('height',bxh);
     }
+    var $infowrap = $('.infowrap');
+    var imgw = $('.imgbx').width(),
+        imgh = $('.imgbx').height();
+    $infowrap.css({
+      'width':imgw,
+      'height':imgh,
+      'margin-top':-imgh
+    });
     setTimeout(whchange,1);
 }
 
 //页面滚动背景色渐变特效
 function pagescroll(){
 
+}
+
+//页面盒子tip
+function boxtips(){
+  var $boxs = $('.thbx');
+  var $boxtips = $('.infowrap');
+  $boxs.each(function(index,element){
+    $(this).on({
+      mouseenter:function(e){
+        e = e || window.event;
+        if(e.stopPropagation){
+             e.stopPropagation();
+        }else{
+             e.cancelBubble=true;
+        }
+        $boxtips.eq(index).fadeIn();
+      }
+    })
+    $(this).on({
+      mouseleave:function(e){
+        e = e || window.event;
+        if(e.stopPropagation){
+             e.stopPropagation();
+        }else{
+             e.cancelBubble=true;
+        }
+        $boxtips.eq(index).fadeOut();
+      }
+    })
+  });
 }
