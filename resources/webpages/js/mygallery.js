3a2982d44d6c222fd8dbdbdd262fddd9dcf90515
MiRECoFu <1027066037@qq.com>
2016-11-10 10:27:15 +0800

    展览馆不同页面自适应

---
+++ resources/webpages/js/mygallery.js
@@ -0,0 +1,12 @@
+$(window).on('load',function(){
+  //logoanimate();
+  pagescroll();
+  var $thbx = $('.thbx');
+  var bxh = $thbx.width()*1.2;
+  $thbx.css('height',bxh);
+})
+
+//页面滚动背景色渐变特效
+function pagescroll(){
+
+}
