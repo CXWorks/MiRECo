3a9823dee2a5e0065b4fab9049fe96b21053c713
CXWorks <1043806046@qq.com>
2017-03-01 01:05:01 +0800

    ..

---
+++ server/database/migrations/2017_02_28_164008_create_paragraph_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParagraphTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('paragraphs', function (Blueprint $table) {
+            //
+            $table->increments('id');
+            $table->string('author');
+            $table->string('title');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('paragraphs');
+    }
+}
